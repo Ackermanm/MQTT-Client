42c50f2319c73e4c215694e526940830e03a8c8e
Xing Meng <u6483085@anu.edu.au>
2020-06-03 03:19:15 +1000

    add external module

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,14 @@
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+
 public class Client {
+    String host = "tcp://comp3310.ddns.net:1883";
+    String clientID = "3310-u6483085";
+    String topic = "$SYS/#";
+    String userName = "students";
+    String password = "33106331";
+    MqttClient client;
+    MqttConnectOptions option;
+
+
 }
